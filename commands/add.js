cab3a6c5ac5518facefaaf64827ae17086083a5c
dexxotri <75505926+dexxotri@users.noreply.github.com>
2020-12-04 14:30:53 -0800

    Add files via upload

---
+++ commands/add.js
@@ -0,0 +1,10 @@
+module.exports = {
+    config: {
+        name: "add",
+        description: "add someone to a Ticket",
+        usage: "rename",
+        category: "ticket",
+        accessableby: "",
+        aliases: [""]
+    },
+}
