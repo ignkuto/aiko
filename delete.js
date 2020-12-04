62576c7ea9e24dab1e80aa726877f7acd72c29ae
dexxotri <75505926+dexxotri@users.noreply.github.com>
2020-12-04 14:16:14 -0800

    Add files via upload

---
+++ delete.js
@@ -0,0 +1,10 @@
+module.exports = { 
+    config: {
+        name: "delete",
+        aliases: [""],
+        description: "Delete a Giveaway",
+        usage: "(messageid)",
+        category: "giveaway",
+        accessableby: "Moderators",
+    }
+}
