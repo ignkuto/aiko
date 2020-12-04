e7de035a62c046361a3bc2d8e210a44c92d5893b
dexxotri <75505926+dexxotri@users.noreply.github.com>
2020-12-04 14:17:39 -0800

    Add files via upload

---
+++ commands/giveaway/reroll.js
@@ -0,0 +1,10 @@
+module.exports = { 
+    config: {
+        name: "reroll",
+        aliases: [""],
+        description: "Reroll a Giveaway",
+        usage: "(messageid)",
+        category: "giveaway",
+        accessableby: "Moderators",
+    }
+}
---
+++ commands/giveaway/start.js
@@ -0,0 +1,10 @@
+module.exports = { 
+    config: {
+        name: "start",
+        aliases: [""],
+        description: "Start a Giveaway",
+        usage: "<channel> (length) (winners) (prize)",
+        category: "giveaway",
+        accessableby: "Moderators",
+    }
+}
